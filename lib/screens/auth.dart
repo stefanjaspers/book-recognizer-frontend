d6111eb65ed935ab0bdb8be57f72b96caace27cf
= <s.jaspers1997@gmail.com>
2023-05-26 21:56:19 +0200

    Implemented user sign-up.

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
@@ -10,6 +13,42 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+
+  var _isLogin = true;
+
+  var _enteredFirstName = '';
+  var _enteredLastName = '';
+  var _enteredUsername = '';
+  var _enteredPassword = '';
+
+  void _submit() async {
+    final isValid = _form.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    if (_isLogin) {
+      // log users in
+    } else {
+      var response = await http.post(Uri.parse('http://localhost:8000/auth/'),
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'username': _enteredUsername,
+            'password': _enteredPassword,
+            'first_name': _enteredFirstName,
+            'last_name': _enteredLastName,
+            'book_preferences': <String>[]
+          }));
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,19 +70,97 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          Visibility(
+                            visible: !_isLogin,
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'First Name'),
+                              keyboardType: TextInputType.name,
+                              autocorrect: false,
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Please enter a valid first name.';
+                                }
+
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredFirstName = value!;
+                              },
+                            ),
+                          ),
+                          Visibility(
+                            visible: !_isLogin,
+                            child: TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Last Name'),
+                              keyboardType: TextInputType.name,
+                              autocorrect: false,
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Please enter a valid last name.';
+                                }
+
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredLastName = value!;
+                              },
+                            ),
+                          ),
                           TextFormField(
                             decoration:
                                 const InputDecoration(labelText: 'Username'),
                             keyboardType: TextInputType.name,
                             autocorrect: false,
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return 'Please enter a valid username.';
+                              }
+
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredUsername = value!;
+                            },
                           ),
                           TextFormField(
                             decoration:
                                 const InputDecoration(labelText: 'Password'),
                             obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be at least 6 characters long.';
+                              }
+
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            },
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: _submit,
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_isLogin ? 'Login' : 'Sign up'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an account'
+                                : 'I already have an account - Login.'),
                           ),
                         ],
                       ),
